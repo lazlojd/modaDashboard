d67c9c1337bddfa83220e86b2f61b5c59f599a3e
Leslie Jones-Dove <ljonesdove@cs.uchicago.edu>
2018-11-11 16:38:26 -0600

    deploy?

---
+++ client/build/precache-manifest.ce16c3bdb7c3ea50a2e382edde0091a9.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb79a6145df8da361415",
+    "url": "/static/css/9.798b6056.chunk.css"
+  },
+  {
+    "revision": "5c587ae171fb12d2c3ba",
+    "url": "/static/js/0.5c587ae1.chunk.js"
+  },
+  {
+    "revision": "dc4c9865eb2b09eadce9",
+    "url": "/static/js/main.dc4c9865.chunk.js"
+  },
+  {
+    "revision": "85f3d3b93390de2e1f57",
+    "url": "/static/js/2.85f3d3b9.chunk.js"
+  },
+  {
+    "revision": "fb1bf27cc7d6013586b0",
+    "url": "/static/js/3.fb1bf27c.chunk.js"
+  },
+  {
+    "revision": "8fb52343c62b6519ba24",
+    "url": "/static/js/4.8fb52343.chunk.js"
+  },
+  {
+    "revision": "45a63af284d8129fea32",
+    "url": "/static/js/5.45a63af2.chunk.js"
+  },
+  {
+    "revision": "b1876e5b2d46c6703474",
+    "url": "/static/js/6.b1876e5b.chunk.js"
+  },
+  {
+    "revision": "b7c0e0fa0f940a79371f",
+    "url": "/static/js/7.b7c0e0fa.chunk.js"
+  },
+  {
+    "revision": "ce2df77a582eb02ca850",
+    "url": "/static/js/8.ce2df77a.chunk.js"
+  },
+  {
+    "revision": "4b57c6a5567c9ef09913983f82904faa",
+    "url": "/static/media/CoreUI-Icons-Linear-Free.4b57c6a5.svg"
+  },
+  {
+    "revision": "eb79a6145df8da361415",
+    "url": "/static/js/9.eb79a614.chunk.js"
+  },
+  {
+    "revision": "680083bea0342defdf79",
+    "url": "/static/js/runtime~main.680083be.js"
+  },
+  {
+    "revision": "1fdfd72de56cb01f75e8227e9489267e",
+    "url": "/static/media/moda-logo.1fdfd72d.jpg"
+  },
+  {
+    "revision": "c8d5c2d9c0e8849271af0664df052817",
+    "url": "/static/media/sygnet.c8d5c2d9.svg"
+  },
+  {
+    "revision": "089ab3c11c572362d088083c561cfa55",
+    "url": "/static/media/CoreUI-Icons-Linear-Free.089ab3c1.eot"
+  },
+  {
+    "revision": "1bc3764815bb391a16771c42c6e460ec",
+    "url": "/static/media/CoreUI-Icons-Linear-Free.1bc37648.ttf"
+  },
+  {
+    "revision": "0087dce419a7376eb06c837f740d31eb",
+    "url": "/static/media/CoreUI-Icons-Linear-Free.0087dce4.woff"
+  },
+  {
+    "revision": "dc4c9865eb2b09eadce9",
+    "url": "/static/css/main.c5d23316.chunk.css"
+  },
+  {
+    "revision": "890dcdba9e9b9c1c7e2c7c5a5dd90f3d",
+    "url": "/index.html"
+  }
+];
